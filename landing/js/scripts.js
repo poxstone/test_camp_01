692031aac12463528b5ccb6587e751754d83167f
poxstone <poxstone@gmail.com>
2019-03-27 21:16:03 -0500

    update js

--- landing/js/scripts.js
+++ landing/js/scripts.js
@@ -4,7 +4,7 @@
     window.APP = new Vue({
         el: '#app',
         data: {
-            validateStatus: true,
+            validateStatus: '',
             isSending: false,
             name: '',
             lastName:'',
@@ -20,19 +20,38 @@
         },
         watch: {
             validateStatus: function(oldVal, newVal) {
-                this.isSending = newVal ? true : false;
+                this.isSending = newVal != '' ? false : true;
             }
         }
     })
 
     // INPUT LOAD FUNCTION
     function ValidateAndSend(event) {
+        event.preventDefault();
         APP.$data.validateStatus = 'validating';
+        var data_json = JSON.stringify(APP.$data);
+        var formObj = event.target;
+        console.log('data to send:', data_json);
+        clearData();
+        
+        setTimeout((x)=>{
+            console.log(APP.$data);
+            alert('Tu información ha sido enviada!' + data_json);
+            APP.$data.validateStatus = '';
+        }, 2000)
     };
 
     function clearData() {
-        APP.$data.validateStatus = false;
-        APP.$data.isSending = false;
+        //APP.$data.validateStatus = '';
+        //APP.$data.isSending = false;
+        APP.$data.name = '';
+        APP.$data.lastName = '';
+        APP.$data.idNumber = '';
+        APP.$data.email = '';
+        APP.$data.phone = '';
+        APP.$data.carModel = '';
+        APP.$data.city = '';
+        APP.$data.concessionaire = '';
     }
 
 })();
